5a63037ae512cfe8c8c8a4dd877449faba4c3cd2
Steven Molina <stevenmolina2205@gmail.com>
2026-02-11 23:19:20 -0300

    chore(): keep only farcaster tenant

--- src/config/tenant.ts
+++ src/config/tenant.ts
@@ -11,31 +11,35 @@ const COINBASE_SUBDOMAINS = ["coinbase.", "web."];
 const BEEXO_SUBDOMAINS = ["beexo.", "mini."];
 
 export const getTenantFromHost = (host: string | null): Tenant => {
-  if (!host) return Tenant.PRIVY;
-
-  const hostname = host.split(":")[0];
-
-  if (BEEXO_SUBDOMAINS.some((subdomain) => hostname.startsWith(subdomain))) {
-    return Tenant.BEEXO;
-  }
-
-  // Use Farcaster Mini App for specific subdomains
-  if (
-    FARCASTER_SUBDOMAINS.some((subdomain) => hostname.startsWith(subdomain))
-  ) {
-    return Tenant.FARCASTER;
-  }
-
-  // Use Coinbase Wallet for specific subdomains
-  if (COINBASE_SUBDOMAINS.some((subdomain) => hostname.startsWith(subdomain))) {
-    return Tenant.COINBASE;
-  }
-
-  // Use Privy for specific subdomains
-  if (PRIVY_SUBDOMAINS.some((subdomain) => hostname.startsWith(subdomain))) {
-    return Tenant.PRIVY;
-  }
-
-  // Default is PRIVY
-  return Tenant.PRIVY;
+  return Tenant.FARCASTER;
 };
+
+// export const getTenantFromHost = (host: string | null): Tenant => {
+//   if (!host) return Tenant.PRIVY;
+
+//   const hostname = host.split(":")[0];
+
+//   if (BEEXO_SUBDOMAINS.some((subdomain) => hostname.startsWith(subdomain))) {
+//     return Tenant.BEEXO;
+//   }
+
+//   // Use Farcaster Mini App for specific subdomains
+//   if (
+//     FARCASTER_SUBDOMAINS.some((subdomain) => hostname.startsWith(subdomain))
+//   ) {
+//     return Tenant.FARCASTER;
+//   }
+
+//   // Use Coinbase Wallet for specific subdomains
+//   if (COINBASE_SUBDOMAINS.some((subdomain) => hostname.startsWith(subdomain))) {
+//     return Tenant.COINBASE;
+//   }
+
+//   // Use Privy for specific subdomains
+//   if (PRIVY_SUBDOMAINS.some((subdomain) => hostname.startsWith(subdomain))) {
+//     return Tenant.PRIVY;
+//   }
+
+//   // Default is PRIVY
+//   return Tenant.PRIVY;
+// };
